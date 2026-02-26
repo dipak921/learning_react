cc4becaf5fdb65fe98886fd30b491d4dac323c5e
dipak921 <dipakchande921@gmail.com>
2026-02-26 15:53:12 +0530

    work is pending

---
+++ Class/Student/src/Footer.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+
+function Footer() {
+  return (
+    <>
+     <footer>
+    <div class="footer-container">
+        <p>© 2026 Your Company Name | All Rights Reserved</p>
+        <div class="footer-links">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+</footer>
+    </>
+  )
+}
+
+
+function Myfoot() {
+  return (
+    <>
+     <footer>
+    <div class="footer-container">
+        <p>© 2026 Your Company Name | All Rights Reserved | Dipak</p>
+       
+    </div>
+</footer>
+    </>
+  )
+}
+
+
+export default Footer
+export { Myfoot }
